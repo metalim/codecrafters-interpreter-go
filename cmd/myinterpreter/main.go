9f822bdc12b25b977e61b008f75bd9724806e161
Maksim Litvinov <metalim@gmail.com>
2024-07-16 16:17:11 +0300

    parse true, false, nil

--- cmd/myinterpreter/main.go
+++ cmd/myinterpreter/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/codecrafters-io/interpreter-starter-go/internal/parser"
 	"github.com/codecrafters-io/interpreter-starter-go/internal/scanner"
 )
 
@@ -17,36 +18,55 @@ func main() {
 
 	command := os.Args[1]
 
-	if command != "tokenize" {
+	switch command {
+
+	case "tokenize":
+		filename := os.Args[2]
+		fileContents, err := os.ReadFile(filename)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error reading file: %v\n", err)
+			os.Exit(1)
+		}
+
+		var exitCode int
+		scan := scanner.New(string(fileContents))
+		go scan.ScanTokens()
+
+		for tok := range scan.Next {
+			if tok.Error != nil {
+				fmt.Fprintf(os.Stderr, "[line %d] Error: %s\n", tok.Line, tok.Error)
+				exitCode = 65
+				continue
+			}
+
+			if len(tok.Literal) == 0 {
+				fmt.Printf("%s %s null\n", tok.Type, tok.Lexeme)
+			} else {
+				fmt.Printf("%s %s %s\n", tok.Type, tok.Lexeme, tok.Literal)
+			}
+		}
+		if exitCode != 0 {
+			os.Exit(exitCode)
+		}
+
+	case "parse":
+		filename := os.Args[2]
+		fileContents, err := os.ReadFile(filename)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error reading file: %v\n", err)
+			os.Exit(1)
+		}
+
+		ast, err := parser.Parse(string(fileContents))
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error parsing file: %v\n", err)
+			os.Exit(1)
+		}
+		ast.Write(os.Stdout)
+
+	default:
 		fmt.Fprintf(os.Stderr, "Unknown command: %s\n", command)
 		os.Exit(1)
 	}
 
-	filename := os.Args[2]
-	fileContents, err := os.ReadFile(filename)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error reading file: %v\n", err)
-		os.Exit(1)
-	}
-
-	var exitCode int
-	scan := scanner.New(string(fileContents))
-	go scan.ScanTokens()
-
-	for tok := range scan.Next {
-		if tok.Error != nil {
-			fmt.Fprintf(os.Stderr, "[line %d] Error: %s\n", tok.Line, tok.Error)
-			exitCode = 65
-			continue
-		}
-
-		if len(tok.Literal) == 0 {
-			fmt.Printf("%s %s null\n", tok.Type, tok.Lexeme)
-		} else {
-			fmt.Printf("%s %s %s\n", tok.Type, tok.Lexeme, tok.Literal)
-		}
-	}
-	if exitCode != 0 {
-		os.Exit(exitCode)
-	}
 }
---
+++ internal/parser/parser.go
@@ -0,0 +1,63 @@
+package parser
+
+import (
+	"errors"
+	"io"
+
+	"github.com/codecrafters-io/interpreter-starter-go/internal/scanner"
+)
+
+func Parse(source string) (*AST, error) {
+	ast := &AST{}
+	scan := scanner.New(source)
+	go scan.ScanTokens()
+
+	for t := range scan.Next {
+		switch t.Type {
+		case scanner.EOF:
+			return ast, nil
+
+		default:
+			ast.nodes = append(ast.nodes, &Keyword{value: t.Lexeme})
+		}
+	}
+	return nil, errors.New("unreachable")
+}
+
+type AST struct {
+	nodes []Node
+}
+
+func (a *AST) Write(w io.Writer) {
+	for _, n := range a.nodes {
+		n.Write(w)
+		io.WriteString(w, "\n")
+	}
+}
+
+type Node interface {
+	Write(io.Writer)
+}
+
+type Keyword struct {
+	value string
+}
+
+func (k *Keyword) Write(w io.Writer) {
+	io.WriteString(w, k.value)
+}
+
+type Binary struct {
+	left  Node
+	right Node
+	op    string
+}
+
+func (b *Binary) Write(w io.Writer) {
+	io.WriteString(w, "(")
+	io.WriteString(w, b.op)
+	b.left.Write(w)
+	io.WriteString(w, " ")
+	b.right.Write(w)
+	io.WriteString(w, ")")
+}
